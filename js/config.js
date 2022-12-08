36121035ae1fa168a9386dd8926b7292d4e7fdaa
andradeexperiments <phiandrade@proton.me>
2022-12-08 01:10:44 -0300

    08/12/2022

--- js/config.js
+++ js/config.js
@@ -7,9 +7,9 @@ const CONFIG = {
    * Update line 11 and 13 if you prefer using Google.
    */
   commands: [{
-      name: 'DuckDuckGO',
+      name: 'SearXNG',
       key: '*',
-      url: 'https://duckduckgo.com',
+      url: 'https://searx.fmac.xyz/',
       search: '/?q={}'
     },
 
