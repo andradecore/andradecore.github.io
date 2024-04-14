7478616217528fddee0f983c8f0ab36559746159
Andrade <phiandrade@proton.me>
2024-04-14 17:22:46 -0300

    14/04/2024

--- js/config.js
+++ js/config.js
@@ -7,9 +7,9 @@ const CONFIG = {
    * Update line 11 and 13 if you prefer using Google.
    */
   commands: [{
-    name: 'SearX',
+    name: 'Google',
     key: '*',
-    url: 'https://searx.daetalytica.io/',
+    url: 'https://www.google.com/',
     search: '/search?q={}'
   },
 
